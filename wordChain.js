7c4213d99ca9225f6d8cf3f8561e5b0ae1f3569a
delta-kor <deltakbot1@gmail.com>
2019-03-23 10:24:20 +0900

    Fixed getWord URL bug

--- wordChain.js
+++ wordChain.js
@@ -1,7 +1,7 @@
 let wc = {
   getWord : function(word){
     var link = "https://api.zz.am/word/"+word
-    var doc = org.jsoup.Jsoup.connect(word)
+    var doc = org.jsoup.Jsoup.connect(link)
     doc = doc.get()
     return doc
   }
