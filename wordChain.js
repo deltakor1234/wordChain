d7d49b13e894a70cce67ad618941cf26579d8d18
delta-kor <deltakbot1@gmail.com>
2019-03-23 10:20:28 +0900

    Added getWord (gets word data)

--- wordChain.js
+++ wordChain.js
@@ -1,5 +1,10 @@
 let wc = {
-  
+  getWord : function(word){
+    var link = "https://api.zz.am/word/"+word
+    var doc = org.jsoup.Jsoup.connect(word)
+    doc = doc.get()
+    return doc
+  }
 }
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
